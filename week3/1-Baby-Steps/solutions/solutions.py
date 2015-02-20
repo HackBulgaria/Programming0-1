fe4772e9863d4f5ea5f1ed6460eccb34131985c0
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-02-20 16:50:45 +0200

    Add comments for the solutions

--- week3/1-Baby-Steps/solutions/solutions.py
+++ week3/1-Baby-Steps/solutions/solutions.py
@@ -1,6 +1,8 @@
 def square(x):
     return x ** 2
 
+
+# Това е същата задача като това да намерим произведението на числата от 1 до n
 def factorial(n):
     result = 1
     start = 1
@@ -11,6 +13,8 @@ def factorial(n):
 
     return result
 
+
+# За всеки елемент в списъка, увеличаваме един брояч
 def count_elements(items):
     result = 0
 
@@ -19,6 +23,10 @@ def count_elements(items):
 
     return result
 
+
+# Използваме флагова променлива
+# В началото казваме, че не сме намерили x
+# Освен ако наистина не го намерим, обхождайки xs
 def member(x, xs):
     found = False
     
@@ -29,6 +37,9 @@ def member(x, xs):
 
     return found
 
+
+# Използваме index-a, за да знаем на кой студент и коя оценка сме
+# По условие знаем, че студентът на index i, има оценка в grades[i]
 def grades_that_pass(students, grades, limit):
     result = []
     index = 0
