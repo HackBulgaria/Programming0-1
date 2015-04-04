ca05db8b9e19cea5be566aa82b6c06640a52acfa
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-04-04 16:49:44 +0300

    Add example for the sets

--- week8/sets.py
+++ week8/sets.py
@@ -15,5 +15,13 @@ def intersection(a, b):
 
 
 def cartesian_product(a, b):
+    # Tuple is (a, b) as a syntax
     pass
 
+
+def main():
+    print(setify([0, 1, 1, 5, 5, 6]) == [0, 1, 5, 6])
+    print(union([0, 0, 0, 0, 0], [1, 2]) == [0, 1, 2])
+    print(intersection([0, 0, 1, 2, 5], [5, 5, 6]) == [5])
+    print(diff([0, 1, 2, 3, 4, 5], [0, 0, 1, 1, 2, 2, 3, 3]) == [4, 5])
+    print(cartesian_product([0, 1], [1]) == [(0, 1), (1, 1)])
